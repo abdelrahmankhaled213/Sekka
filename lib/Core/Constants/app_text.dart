869191e15227e32773223f99766f169cb5712a00
LAPTOP-N2P6TMJ2\lenovo <abdelrahmanseif070@gmail.com>
2026-03-01 01:19:46 +0200

    Adding App Text

---
+++ lib/Core/Constants/app_text.dart
@@ -0,0 +1,32 @@
+abstract class AppText{
+
+  static const String backToLogin="Back to login";
+  static const String createAccount="Create Account";
+  static const String joinSekkaAndTravelSmart="Join Sekka and travel smart ";
+  static const String phone="Phone";
+  static const String email="Email";
+  static const String fullName="Full Name";
+  static const String phoneNumber="Phone Number";
+  static const String emailAddress="Email Address";
+  static const String password="Password";
+  static const String confirmPassword="Confirm Password";
+  static const String enterYourName="Enter your full name";
+  static const String enterYourEmail="Enter your email";
+  static const String enterYourPhoneNumber="Enter your phone number";
+  static const String enterYourPassword="Create a password";
+  static const String enterYourConfirmPassword="Confirm your password";
+  static const String passwordMustContain="Password must contain :";
+  static const String validationSpecialCharacter="has at least 1 character";
+  static const String validationUpperCase="has at least 1 uppercase letter";
+  static const String validationLowerCase="has at least 1 lowercase letter";
+  static const String validationTextLength="has at least 8 characters long";
+  static const String validationNumber="has at least 1 number";
+  static const String validationPhone="Is phone valid";
+  static const String iAgreeToThe="I agree to the";
+  static const String termsOfService="Terms of Service";
+  static const String and="and";
+  static const String privacyPolicy="Privacy Policy";
+
+
+
+}
