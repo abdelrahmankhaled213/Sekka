ebd4a2573686682c6e700f993348f6d2258c6cee
LAPTOP-N2P6TMJ2\lenovo <abdelrahmanseif070@gmail.com>
2026-03-01 01:13:31 +0200

    Adding Observer and Orientations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sekka/Core/App/env_variables.dart';
 import 'package:sekka/Core/DI/service_locator.dart';
+import 'package:sekka/Core/Helper/bloc_observer_helper.dart';
 import 'package:sekka/sekka.dart';
 
 void main() async{
@@ -8,6 +11,11 @@ void main() async{
   final instance=EnvironmentVariable.instance;
   await instance.init(EnvironmentVariableEnum.dev);
   setUpServiceLocator();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  Bloc.observer=MyBlocObserver();
   runApp(const Sekka());
 }
 
