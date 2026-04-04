f5dae0be6b60df29c302cf64a5ad964a4a8eb9b8
LAPTOP-N2P6TMJ2\lenovo <abdelrahmanseif070@gmail.com>
2026-04-04 15:05:53 +0200

    fix railway port

--- server.js
+++ server.js
@@ -56,7 +56,8 @@ app.post("/saveToken", async (req, res) => {
 });
 
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
