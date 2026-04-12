e19b963aded1a9ee3fcc948a7f98986ea8d24916
LAPTOP-N2P6TMJ2\lenovo <abdelrahmanseif070@gmail.com>
2026-04-12 14:37:09 +0200

    test_notification endpoint

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+require("dotenv").config()
 
 const serviceAccount = JSON.parse(process.env.FirebaseServiceAccount);
+
+const { createClient } = require("@supabase/supabase-js")
+
+const supabase= createClient(
+  process.env.SUPABASE_URL
+  , process.env.SUPABASE_KEY);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -17,12 +25,33 @@ const db = admin.firestore();
 
 
 app.get("/", (req, res) => {
-  res.send("Transport backend running 🚀");
+  
+  res.send("dah hosting free 🚀");
+
+});
+
+app.post("/test-notification"
+  ,async(req,res)=>{
+
+  const {token,title,body}=req.body;
+  try{
+    await sendNotification(
+      token
+      ,title
+      ,body,
+    );
+
+    res.json({ success: true, message: "Notification sent successfully" });
+  }catch(e){
+    res.status(500).json({ error: e.message });
+  }
+
 });
 
 
 app.post("/addStation", async (req, res) => {
   try {
+
     const { name, lat, lng } = req.body;
 
     const doc = await db.collection("stations").add({
@@ -40,22 +69,23 @@ app.post("/addStation", async (req, res) => {
   }
 });
 
+async function sendNotification(token,title, body) {
 
-app.post("/saveToken", async (req, res) => {
-  try {
-
-    const { userId, token } = req.body;
-
-    await db.collection("users").doc(userId).set({
-      fcmToken: token,
-    });
-
-    res.json({ success: true });
-  } catch (e) {
-    res.status(500).json({ error: e.message });
+  const message = {
+   notification:{
+    title:title,
+    body:body
+   },
+   token:token
   }
-});
+  try {
+    await admin.messaging().send(message);
+    console.log("Notification sent successfully");
+  } catch (error) {
+    console.error("Error sending notification:", error);
+  }
 
+}
 
 const PORT = process.env.PORT || 3000;
 
